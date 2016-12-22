3cc3026a1894c158b7bc8de06c65e5dab959e5e3
HolyAngel <slverwolf@gmail.com>
2016-12-22 11:00:09 -0600

    Add initial supersu and fixes
    Change-Id: I529f9b9ffcaed8b67e9ef7354bd4f29142528dea
    Signed-off-by: HolyAngel <slverwolf@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -19,10 +19,40 @@ endif
 
 # general properties
 PRODUCT_PROPERTY_OVERRIDES += \
+    keyguard.no_require_sim=true \
     ro.url.legal=http://www.google.com/intl/%s/mobile/android/basic/phone-legal.html \
+    ro.url.legal.android_privacy=http://www.google.com/intl/%s/mobile/android/basic/privacy.html \
     ro.com.android.wifi-watchlist=GoogleGuest \
     ro.setupwizard.enterprise_mode=1 \
+ro.com.android.dateformat=MM-dd-yyyy \
+    ro.com.android.dataroaming=false \
+    ro.error.receiver.system.apps=com.google.android.gms \
+    ro.atrace.core.services=com.google.android.gms,com.google.android.gms.ui,com.google.android.gms.persistent
+    
+# SkyDragon property overides
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.sys.fw.bg_apps_limit=20 \
+    windowsmgr.max_events_per_sec=150 \
+    debug.performance.tuning=1 \
+    ro.ril.power_collapse=1 \
+    persist.service.lgospd.enable=0 \
+    persist.service.pcsync.enable=0 \
+    ro.facelock.black_timeout=400 \
+    ro.facelock.det_timeout=1500 \
+    ro.facelock.rec_timeout=2500 \
+    ro.facelock.lively_timeout=2500 \
+    ro.facelock.est_max_time=600 \
+    ro.facelock.use_intro_anim=false \
+    ro.setupwizard.network_required=false \
+    ro.setupwizard.gservices_delay=-1 \
+    net.tethering.noprovisioning=true \
+    persist.sys.dun.override=0 \
+    ro.substratum.verified=true
     persist.sys.root_access=1
+
+# Google Assistant
+ PRODUCT_PROPERTY_OVERRIDES += \
+    ro.opa.eligible_device=true
 
 # enable ADB authentication if not on eng build
 ifneq ($(TARGET_BUILD_VARIANT),eng)
@@ -39,6 +69,10 @@ PRODUCT_COPY_FILES += \
 # Backup Services whitelist
 PRODUCT_COPY_FILES += \
     vendor/omni/prebuilt/etc/sysconfig/backup.xml:system/etc/sysconfig/backup.xml
+
+# SuperSU
+PRODUCT_COPY_FILES += \
+    vendor/omni/prebuilt/UPDATE-SuperSU.zip:system/addon.d/UPDATE-SuperSU.zip 
 
 # init.d support
 PRODUCT_COPY_FILES += \
